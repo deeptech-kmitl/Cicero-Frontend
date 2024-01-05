723fad37315f1ccea6b7a7c5736b159d1c09a133
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-01-05 21:28:19 +0700

    setup project next + shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen justify-center flex-col items-center  p-24">
+      <h1 className='bg-slate-600 p-2 text-white rounded-lg mb-3'>XD This is a Test Cicero XD</h1>
+      <Button>Test for GIGA SHADCN</Button>
+    </main>
+  )
+}
