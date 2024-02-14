3fc2c1e15a418cbbbbee4909ed066b6c505f9c34
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2024-02-15 03:50:04 +0700

    render item

--- src/app/(site)/homepage/page.tsx
+++ src/app/(site)/homepage/page.tsx
@@ -1,39 +1,108 @@
 import React from "react";
 import CardHomePage from "@/components/homePage/card"
 import { Button } from "@/components/ui/button";
-import Image from 'next/image';
 
 type Props = {};
 
-const Homepage = () => {
+const mockData = [
+  {
+    image: "",
+    name: "THE STAR CROP VEST",
+    price: "1,590",
+    favStatus: false,
+    category: "Tops",
+    sex: "women",
+    size: "S",
+    color: "pink",
+    productId: "PD123456",
+  },
+  {
+    image: "",
+    name: "THE STAR CROP VEST",
+    price: "1,590",
+    favStatus: false,
+    category: "Tops",
+    sex: "women",
+    size: "S",
+    color: "pink",
+    productId: "PD123456",
+  },
+  {
+    image: "",
+    name: "THE STAR CROP VEST",
+    price: "1,590",
+    favStatus: false,
+    category: "Tops",
+    sex: "women",
+    size: "S",
+    color: "pink",
+    productId: "PD123456",
+  },
+  {
+    image: "",
+    name: "THE STAR CROP VEST",
+    price: "1,590",
+    favStatus: false,
+    category: "Tops",
+    sex: "women",
+    size: "S",
+    color: "pink",
+    productId: "PD123456",
+  },
+  {
+    image: "",
+    name: "THE STAR CROP VEST",
+    price: "1,590",
+    favStatus: false,
+    category: "Tops",
+    sex: "women",
+    size: "S",
+    color: "pink",
+    productId: "PD123456",
+  },
+];
+
+
+const Homepage = (props: Props) => {
   return (
     <div>
       <div className="flex h-full justify-center flex-col items-center p-10">
         <div className="w-11/12 flex justify-center flex-col items-center">
-          <div className="h-96 w-11/12 bg-black"></div>
+          <div className="h-96 w-11/12 bg-gray-200"></div>
           <div className="flex flex-row w-11/12 mt-7 gap-2">
               <div className="w-full h-52 flex justify-center items-center bg-[url('https://i.ibb.co/6N7Z1qz/Rectangle-7.png')] bg-cover">
                 <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none ab">
                   FOR WOMEN
                 </Button>
               </div>
-              <div className="w-full h-52 bg-red-200 flex justify-center items-center">
+              <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/dM68xRs/image-30.png')] bg-cover">
                 <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
                   FOR MEN
                 </Button>
               </div>
-              <div className="w-full h-52 bg-red-200 flex justify-center items-center">
+              <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/QK3PZdn/Rectangle-4.png')] bg-cover">
                 <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
                   FOR KID
                 </Button>
               </div>
           </div>
-          <div className="flex flex-row w-11/12 mt-7 gap-4">
-            <CardHomePage/>
-            <CardHomePage/>
-            <CardHomePage/>
-            <CardHomePage/>
-            <CardHomePage/>
+          <div className="grid grid-cols-5 w-11/12 mt-7 gap-4">
+          {mockData.map((item, i) => {
+            return (
+              <CardHomePage
+                key={i}
+                image={item.image}
+                name={item.name}
+                price={item.price}
+                favStatus={item.favStatus}
+                category={item.category}
+                sex={item.sex}  
+                size= {item.size}
+                color={item.color}
+                productId={item.productId}
+              />
+            );
+          })}
           </div>
           <div className="h-[28rem] w-11/12 bg-black mt-5 bg-[url('https://i.ibb.co/bsXk3pS/Rectangle-12.png')] bg-cover flex justify-end p-6">
               <Button className="h-[13%] w-[13%] bg-black text-center justify-center flex text-white rounded-none ab">
--- src/components/homePage/card.tsx
+++ src/components/homePage/card.tsx
@@ -2,21 +2,38 @@ import React from 'react'
 import Image from 'next/image';
 import { Heart } from "lucide-react";
 
-// type CardHomePageProps = {
-    
-// }
+type ItemProduct = {
+  image: string
+  name: string
+  price: string
+  favStatus: boolean
+  category: string
+  sex: string
+  size: string
+  color: string
+  productId: string
+};
 
 
-const CardHomePage = () => {
+const CardHomePage = (props: ItemProduct) => {
   return (
     <div className="flex flex-col h-[25rem] w-full">
-      <div className='h-[80%] w-full bg-black'></div>
+      <div className='h-[80%] w-full bg-gray-200'>
+        <Image
+          className="h-[100%] w-[100%]"
+          src={props.image}
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="product"
+        />
+      </div>
       <div className='flex flex-row mt-2'>
-        <div className='basis-10/12 text-base'>THE STAR CROP VEST</div>
+        <div className='basis-10/12 text-base'>{props.name}</div>
         <Heart className='basis-2/12'/>
       </div>
       <div className='flex flex-row'>
-        <div className='text-xl'>1,590</div>
+        <div className='text-xl'>{props.price}</div>
         <div className='text-sm ml-1 mt-1.5 text-gray-400'>THB</div>
       </div>
     </div>
