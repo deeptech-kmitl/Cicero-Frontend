75b6da7ce8168b9e7c60e3590547dbec4f20482f
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-02-15 18:01:04 +0700

    ♻️ refactor : Refactor auth.ts module

--- src/api-caller/auth.ts
+++ src/api-caller/auth.ts
@@ -1,38 +1,34 @@
 import { getInstance } from "@/api/apiClient";
 import { IFormattedErrorResponse, IFormattedSuccessResponse, SignInResponse, User } from "@/constants/interface";
-import { formattedError, formattedSuccess } from "@/lib/utils";
-import { AxiosResponse } from "axios";
-import { cookies } from "next/headers";
+import { formattedError } from "@/lib/utils";
 
-export default async function signIn(form: FormData): Promise<IFormattedSuccessResponse<SignInResponse> | IFormattedErrorResponse> {
+export default async function signIn(form: FormData): Promise<SignInResponse | IFormattedErrorResponse> {
   try {
-    const {data,status,statusText} : AxiosResponse<SignInResponse> = await getInstance().post("/users/signin/", form);
-    const res = formattedSuccess<SignInResponse>(data,status,statusText);
-    console.log(res)
-    return res;
+    const {data} = await getInstance().post("/users/signin/", form);
+    return data;
   } catch (error) {
-    return  formattedError(error);
+    return formattedError(error);
   }
 }
 
-export async function signUp(form: FormData): Promise<IFormattedSuccessResponse<User> | IFormattedErrorResponse> {
+export async function signUp(form: FormData): Promise<User | IFormattedErrorResponse> {
     try {
-      const { data, status, statusText } = await getInstance().post("/users/signup/", form);
-      const res = formattedSuccess<User>(data, status, statusText);
-      return res;
+      const {data}= await getInstance().post("/users/signup/", form);
+     return data;
     } catch (error) {
       return formattedError(error);
     }
   }
 
-export async function getProfile(id: string, token:string): Promise<any> {
+///test send cookies
+export async function getProfile(id: string,token: string): Promise<User> {
   try {
-    const { data, status } = await getInstance().get(`/users/${id}`, {
-      headers:{
-        Authorization: `Bearer ${token}`
+    const { data } = await getInstance().get(`/users/${id}`, {
+      headers: {
+        "Authorization": token,
       }
     });
-    return { data, status };
+    return data;
   } catch (error) {
     throw formattedError(error);  
   }
