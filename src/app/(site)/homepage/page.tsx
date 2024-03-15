4bcc03f6f5229673c809270cdb51c2549b51dc6d
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2024-03-15 20:43:30 +0700

    implement browse product

--- src/app/(site)/homepage/page.tsx
+++ src/app/(site)/homepage/page.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import CardHomePage from "@/components/homePage/card"
 import { Button } from "@/components/ui/button";
+import { getAllProduct } from "@/api-caller/product";
 
 type Props = {};
 
@@ -65,6 +67,15 @@ const mockData = [
 
 const Homepage = (props: Props) => {
 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProduct().then((data) => { 
+      setProducts(data) 
+      console.log(data)
+    })
+  }, [])
+
   return (
     <div>
       <div className="flex h-full justify-center flex-col items-center p-10">
@@ -73,55 +84,55 @@ const Homepage = (props: Props) => {
             <div className="text-[12rem] font-extrabold text-white tracking-[0.2em] opacity-50">CICERO</div>
           </div>
           <div className="flex flex-row w-11/12 mt-7 gap-2">
-              <div className="w-full h-52 flex justify-center items-center bg-[url('https://i.ibb.co/6N7Z1qz/Rectangle-7.png')] bg-cover">
-                <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none ab">
-                  FOR WOMEN
-                </Button>
-              </div>
-              <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/dM68xRs/image-30.png')] bg-cover">
-                <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
-                  FOR MEN
-                </Button>
-              </div>
-              <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/QK3PZdn/Rectangle-4.png')] bg-cover">
-                <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
-                  FOR KID
-                </Button>
-              </div>
+            <div className="w-full h-52 flex justify-center items-center bg-[url('https://i.ibb.co/6N7Z1qz/Rectangle-7.png')] bg-cover">
+              <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none ab">
+                FOR WOMEN
+              </Button>
+            </div>
+            <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/dM68xRs/image-30.png')] bg-cover">
+              <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
+                FOR MEN
+              </Button>
+            </div>
+            <div className="w-full h-52 bg-red-200 flex justify-center items-center bg-[url('https://i.ibb.co/QK3PZdn/Rectangle-4.png')] bg-cover">
+              <Button className="h-[20%] w-[30%] bg-black text-center justify-center flex text-white rounded-none">
+                FOR KID
+              </Button>
+            </div>
           </div>
           <div className="grid grid-cols-5 w-11/12 mt-7 gap-5">
-          {mockData.map((item, i) => {
-            return (
-              <CardHomePage
-                key={i}
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                favStatus={item.favStatus}
-                category={item.category}
-                sex={item.sex}  
-                size= {item.size}
-                color={item.color}
-                productId={item.productId}
-              />
-            );
-          })}
+            {mockData.map((item, i) => {
+              return (
+                <CardHomePage
+                  key={i}
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                  favStatus={item.favStatus}
+                  category={item.category}
+                  sex={item.sex}
+                  size={item.size}
+                  color={item.color}
+                  productId={item.productId}
+                />
+              );
+            })}
           </div>
           <div className="h-[28rem] w-11/12 bg-black mt-5 bg-[url('https://i.ibb.co/bsXk3pS/Rectangle-12.png')] bg-cover flex justify-end p-6">
-              <Button className="h-[13%] w-[13%] bg-black text-center justify-center flex text-white rounded-none ab">
-                 BUY NOW
-              </Button>
+            <Button className="h-[13%] w-[13%] bg-black text-center justify-center flex text-white rounded-none ab">
+              BUY NOW
+            </Button>
           </div>
           <div className="h-[28rem] w-11/12 bg-black mt-5 bg-[url('https://i.ibb.co/VNcFyhC/Rectangle-4227.png')] bg-cover flex justify-end p-6">
-              <Button className="h-[13%] w-[13%] bg-black text-center justify-center flex text-white rounded-none ab">
-                 BUY NOW
-              </Button>
+            <Button className="h-[13%] w-[13%] bg-black text-center justify-center flex text-white rounded-none ab">
+              BUY NOW
+            </Button>
           </div>
-          
+
         </div>
       </div>
     </div>
-    
+
   );
 };
 
