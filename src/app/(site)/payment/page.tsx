5d3c64ec0e67329dc1daabcea1110aaccab7942f
phufarpsw <63070137@it.kmitl.ac.th>
2024-03-24 21:24:36 +0700

    format payment

--- src/app/(site)/payment/page.tsx
+++ src/app/(site)/payment/page.tsx
@@ -1,25 +1,24 @@
-"use client"
-import CartFooter from '@/components/cart/CartFooter'
-import Topbar from '@/components/cart/Topbar'
-import PaymentForm from '@/components/payment/PaymentForm'
-import React from 'react'
-import { destr } from 'destr';
-import { IUser } from '@/constants/interface';
-import { cookies } from 'next/headers'
+"use client";
+import CartFooter from "@/components/cart/CartFooter";
+import Topbar from "@/components/cart/Topbar";
+import PaymentForm from "@/components/payment/PaymentForm";
+import React from "react";
+import { destr } from "destr";
+import { IUser } from "@/constants/interface";
+import { cookies } from "next/headers";
 
-type Props = {}
+type Props = {};
 
 const Payment = (props: Props) => {
-	const user = destr<IUser>(cookies().get('user')?.value || '{}')
-	const token = cookies().get('token')?.value || ''
+  const user = destr<IUser>(cookies().get("user")?.value || "{}");
+  const token = cookies().get("token")?.value || "";
   return (
     <div className="flex px-10 container flex-col justify-center pt-[5%] space-y-7 items-center">
-			<Topbar page="payment" />
+      <Topbar page="payment" />
+      <PaymentForm user_id={user.id} token={token} />
+      {/* <CartFooter /> */}
+    </div>
+  );
+};
 
-				<PaymentForm user_id={user.id} token={token} />
-			{/* <CartFooter /> */}
-		</div>
-  )
-}
-
-export default Payment
+export default Payment;
